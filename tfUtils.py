Fixed a sign error in tfUtils.  Basically gainAndPhaseToComplex() and complexToPhaseAndGain() weren't following the same sign convention because I just assumed what np.angle() was doing (assuming is dumb)

tfUtils.py
@@ -85,7 +85,7 @@ def calcLogMag(graphF,graphFFT):
     time-integral squared amplitude
 
     
-    I somehow messed this up
+    I somehow messed this up?
     """
 
     dF = (graphF[1]-graphF[0])
@@ -405,17 +405,16 @@ def regenerateCableLinMag(f,linMag,dF=5000./512.,length=513):
 
 
 
-def gainAndPhaseToComplex(gainLin,phase):
-    real = []
-    imag = []
-    for i in range(0,len(gainLin)):
-        real.append(gainLin[i]*np.sin(phase[i]))
-        imag.append(gainLin[i]*np.cos(phase[i]))
-  
-    return np.array(real)+np.array(imag)*1j
-
 
 def complexToGainAndPhase(fft):
+    """
+    the gain and phase are polar representations of the real/imag values on the complex plane
+
+    np.angle = "the counterclockwise angle from the positive real axis on the complex plane"
+
+    That seems like a fine convention to stick to, I'm sure other people use it too
+    """
+
     gainLin = []
     phase = []
     for i in range(0,len(fft)):
@@ -426,6 +425,21 @@ def complexToGainAndPhase(fft):
 #    phase = unwrapPhase(phase)
 
     return np.array(gainLin),np.array(phase)
+
+
+def gainAndPhaseToComplex(gainLin,phase):
+    """
+    Just need to follow the conventions from complexToGainAndPhase() which is defined by np.angle
+    """
+
+    real = []
+    imag = []
+    for i in range(0,len(gainLin)):
+        imag.append(gainLin[i]*np.sin(phase[i]))
+        real.append(gainLin[i]*np.cos(phase[i]))
+  
+    return np.array(real)+np.array(imag)*1j
+
 
 
 

transferFunctionFinal.py
@@ -712,12 +712,12 @@ def doPalAnt(chan):
     antTFFFT = tf.sqrtOfFFT2(antTFFFT)
 
 
-    #Make causal?
-    antTFFFT = tf.makeCausalFFT(antTFFFT,np.argmax(tf.fftw.irfft(antTFFFT)))
+    #Make causal?  no probably not, that seems like a "last thing you do" sort of technique
+#    antTFFFT = tf.makeCausalFFT(antTFFFT,np.argmax(tf.fftw.irfft(antTFFFT)))
 
-    #I have to get the time domain again after that, which is time reversed because of math
+    #I have to get the time domain again after that
     antTFY = tf.fftw.irfft(antTFFFT)
-    
+
     
     #clean up the tail and make it start at the beginning
     print "doing hanning window"
@@ -726,7 +726,7 @@ def doPalAnt(chan):
     antTFFFT = tf.fftw.rfft(antTFY)
 
 
-    return antTFX,antTFY,antTFF,antTFFFT
+    return antTFX,antTFY#,antTFF,antTFFFT
 
 
 
@@ -839,6 +839,47 @@ def doTheWholeShebang():
     writeAll(allChans)
 
     return allChans
+
+#####################
+def doAllSigChains():
+    """
+    just does all the signal chains without the antennas
+    """
+    chans = np.loadtxt("chanList.txt",dtype=str)
+    allChans = {}
+    for chan in chans:
+        try:
+            allChans[chan] = doSigChainWithCables(chan)
+        except:
+            print chan+" FAILED************************************************"
+
+    return allChans
+
+####################
+def doAllAntennas():
+    """
+    just does all the palestine antennas
+    """
+    chans = np.loadtxt("chanList.txt",dtype=str)
+    allChans = {}
+    for chan in chans:
+        try:
+            allChans[chan] = doPalAnt(chan)
+        except:
+            print chan+" FAILED************************************************"
+
+    return allChans
+
+
+def plotAllAntennas(allAnts):
+    fig,ax = lab.subplots(2)
+    for chan in allAnts:
+        if chan[-1] == "H":
+            ax[0].plot(allAnts[chan][0],allAnts[chan][1])
+        else:
+            ax[1].plot(allAnts[chan][0],allAnts[chan][1])
+
+    fig.show()
 
 
 ########################