Fixed scaling under calcGroupDelay().

tfUtils.py
@@ -482,7 +482,7 @@ def calcGroupDelay(inputFFT,inputF=-1,dF=-1):
 
 def calcGroupDelayFromPhase(phase,dF=1):
 
-    GrpDly = -np.diff(phase)/2*np.pi*dF
+    GrpDly = -np.diff(phase) / (2 * np.pi * dF)
 
     return GrpDly
 
@@ -568,7 +568,7 @@ def printTimeDomainToFile(x,y,file):
     return
 
 
-def convolve(grA,grB):
+def convolve(grA, grB):
     """
     I should write my own deconvolution algorithm if it is so easy
 
@@ -1160,7 +1160,6 @@ def phaseShiftMovie(save=False):
     compPhaseShifts3(waveY,len(waveY),save=save)
 
 
-
 def compPhaseShifts3(y=[],center=[],save=False):
 
     #set up defaults
@@ -1172,14 +1171,12 @@ def compPhaseShifts3(y=[],center=[],save=False):
         center = len(y)/2
 
 
-
     #get the fourier domain
     fft = fftw.rfft(y)
 
     #From Peter:  For group delay you should choose the mean centroid of the pulses as the reference time, then the delay skew will be minimized. The phase plot will be redundant once you get the group delay tuned correctly (eg. dphi/domega is more useful than phi(omega))
     # so I need to 
     
-
     maxes  = []
     mins   = []
     maxLoc = []
@@ -1219,7 +1216,6 @@ def compPhaseShifts3(y=[],center=[],save=False):
         ax[2].plot(np.angle(shiftedFFT[1:]))
         ax[2].set_ylabel("Phase (radians)")
         ax[2].set_xlabel("Frequency (arbitrary)")
-        
 
 #        ax[1][0].plot(10.*np.log10((np.abs(shiftedFFT[1:-1])**2)))
 
@@ -1292,7 +1288,6 @@ def compPhaseShifts4(y):
     return
 
 
-
 def minimizeGroupDelay():
 
     wave = np.loadtxt("autoPlots/15TV.txt")
@@ -1300,7 +1295,7 @@ def minimizeGroupDelay():
     waveX = wave.T[0]
     waveY = wave.T[1]
 
-    f,fft = genFFT(waveX,waveY)
+    f,fft = genFFT(waveX, waveY)
 
     phase = calcPhase(fft)
     gdOrig = calcGroupDelay(fft)
@@ -1322,7 +1317,6 @@ def minimizeGroupDelay():
     phaseNew = phase - phaseCorr
     gdNew  = calcGroupDelayFromPhase(phaseNew)
 
-
     axes[1].set_xlabel("Frequency (GHz)")
     axes[1].set_ylabel("Group Dleay (ns)")
     axes[1].plot(f[1:],gdNew,label="corrected group delay")
@@ -1332,9 +1326,6 @@ def minimizeGroupDelay():
     axes[1].legend(loc="lower left")
     twinAx.legend()
 
-
     figs.show()
 
     return
-                         
-